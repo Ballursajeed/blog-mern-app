369425ef5de086e70f42a1bac9959fa1faa4ab96
Ballursajeed <ballursajeed@gmail.com>
2023-12-25 09:36:05 +0530

    updated the server link ror deployment

--- backend/index.js
+++ backend/index.js
@@ -20,13 +20,7 @@ const allowedOrigins = [
 // Configure CORS with specific origin(s)
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
+    origin: 'https://blog-mern-app-client.vercel.app'
   })
 );
 app.use(express.json())
